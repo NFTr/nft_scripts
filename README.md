a6095f75bf521dbd079ffeb6ad4d39ccebd57269
BH <brandtholmes@gmail.com>
2022-11-10 14:03:22 -0700

    Creation and upload of metadata updater script

--- README.md
+++ README.md
@@ -7,5 +7,19 @@ This script converts the metadata created by the generative art software [hashli
 
 To use this script you will need to identify the file locations where Hashlips created the metadata files then fill in the fields within the script pertaining to your project.
 
+Open this script to review where and how files need to be stored, contact the NFTr team with any questions.
+
 ### Contributors:
-This script was originally created by Maxim Goods and later updated by the MonkeyZoo team, the NFTr team, and a number of other devs. Message ClydeWallace if you would like to be included in the contributors list.
+This script was originally created by Maxim Goods and later updated by the MonkeyZoo team, the NFTr team, and a number of other devs. Message the NFTr team if you would like to be included in the contributors list.
+
+## metadata attributes updater
+This script updates the values of attributes based on an update.csv file.
+The update.csv file should contain the old and new values separated by a comma (do not use spaces around the comma)
+All of the metadata files you need to update should be in a folder named "old_metadata"
+This script and update.csv file will need to be in the same directory as your old_metadata folder
+
+Contact the NFTr team with any questions.
+
+### Contributors:
+This script was originally created by ClydeWallace for use by the MonkeyZoo team.
+Message the NFTr team if you would like to be included in the contributors list.
---
+++ hashlips/metadata_updater.py
@@ -0,0 +1,55 @@
+import json
+import csv
+import os
+import shutil
+
+#file paths for metadata, make sure old metadata is in old_metadata folder the new folder will be created automatically
+json_directory_in = 'old_metadata'
+json_directory_out = 'metadata'
+
+#file containing update list (first value is old attribute being replaced, second is attribute replacing the first)
+csv_path = 'update.csv'
+
+#lists used to store the change data
+old_strings = []
+new_strings = []
+
+#index for tracking what is being replaced
+a = 0
+
+#opens, reads, and assigns update values from update.csv
+with open(csv_path, 'r') as changes:
+    data = csv.reader(changes, delimiter = ',')
+    updates = next(data)
+    for row in data:
+        old_strings.append(row[0])
+        new_strings.append(row[1])
+    print(len(old_strings)," attributes are being checked for replacement") #comment to show user that things are happening
+
+#checks if new metadata folder is present, if not it will be created
+if not os.path.exists("metadata"):
+    os.makedirs("metadata")
+
+#starts metadata file iteration
+for filename in os.listdir(json_directory_in):
+    f = os.path.join(json_directory_in, filename)
+    if os.path.isfile(f):
+        filename_json_in = os.path.splitext(filename)[0] + ".json"
+        filename_json_out = os.path.splitext(filename)[0] + ".json"
+        print("checking file: ", filename)    # output to user that we're making progress, can be commented out
+
+        #opens metadata file for checking
+        with open(os.path.join(json_directory_in, filename_json_in), 'r') as mfile:
+            json_data = json.load(mfile)
+            for d in json_data['attributes']:
+                print("original value: ", d['value'])
+                a = 0
+                while a < len(new_strings):
+                    if(d['value'].lower()) == old_strings[a].lower(): #added to ensure capital letters in the old values do not break script
+                        d['value'] = new_strings[a] #updates old values to new values
+                        print("new value: ", new_strings[a])
+                    a = a + 1
+
+        #writes new values to file
+        with open(os.path.join(json_directory_out, filename_json_out), 'w') as file:
+            json.dump(json_data, file, indent=2)
